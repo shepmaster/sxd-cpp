f405b81ea4b622b7739ffb628edc94e34950fcf6
Jake Goulding <jake.goulding@gmail.com>
2010-09-18 23:32:18 -0400

    move one test to cpputest

--- intern-test.cpp
+++ intern-test.cpp
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
+extern "C" {
 #include "intern.h"
+}
 
-static void
-test_new_intern(void)
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+
+TEST_GROUP(intern)
+{};
+
+TEST(intern, new1)
 {
   intern_t *i;
   i = intern_new();
-  assert(i != NULL);
+  CHECK(i != NULL);
   intern_free(i);
 }
 
-static void
-test_intern_const_string(void)
+TEST(intern, const_string)
 {
   intern_t *i;
   const char * const input = "hello";
@@ -24,15 +29,14 @@ test_intern_const_string(void)
   i = intern_new();
 
   result = intern_intern(i, input);
-  assert(result != NULL);
-  assert(strcmp(result, input) == 0);
-  assert(result != input);
+  CHECK(result != NULL);
+  STRCMP_EQUAL(input, result);
+  CHECK(result != input);
 
   intern_free(i);
 }
 
-static void
-test_intern_allocated_string(void)
+TEST(intern, allocated_string)
 {
   intern_t *i;
   char *input;
@@ -42,16 +46,15 @@ test_intern_allocated_string(void)
   input = strdup("hello");
 
   result = intern_intern(i, input);
-  assert(result != NULL);
-  assert(strcmp(result, input) == 0);
-  assert(result != input);
+  CHECK(result != NULL);
+  STRCMP_EQUAL(input, result);
+  CHECK(result != input);
 
   free(input);
   intern_free(i);
 }
 
-static void
-test_intern_two_identical_strings(void)
+TEST(intern, two_identical_strings)
 {
   intern_t *i;
   char *input1;
@@ -66,13 +69,13 @@ test_intern_two_identical_strings(void)
   result1 = intern_intern(i, input1);
   result2 = intern_intern(i, input2);
 
-  assert(result1 != NULL);
-  assert(result2 != NULL);
+  CHECK(result1 != NULL);
+  CHECK(result2 != NULL);
 
-  assert(strcmp(result1, input1) == 0);
-  assert(strcmp(result2, input2) == 0);
-  assert(strcmp(result1, result2) == 0);
-  assert(result1 == result2);
+  STRCMP_EQUAL(input1, result1);
+  STRCMP_EQUAL(input2, result2);
+  STRCMP_EQUAL(result2, result1);
+  POINTERS_EQUAL(result1, result2);
 
   free(input1);
   free(input2);
@@ -82,10 +85,5 @@ test_intern_two_identical_strings(void)
 int
 main(int argc, char **argv)
 {
-  test_new_intern();
-  test_intern_const_string();
-  test_intern_allocated_string();
-  test_intern_two_identical_strings();
-
-  return EXIT_SUCCESS;
+  return CommandLineTestRunner::RunAllTests(argc, argv);
 }
