dd0f6f81151bee9eb9013d5837f4090605e0f126
Jake Goulding <jake.goulding@gmail.com>
2013-02-01 12:13:20 -0600

    Update comment test to use google-test

--- comment-test.cpp
+++ comment-test.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
-
-#include <CppUTest/TestHarness_c.h>
-#include <CppUTest/CommandLineTestRunner.h>
-
-#include "document.h"
 #include "comment.h"
 
-TEST_GROUP(comment)
-{
+#include "document.h"
+#include "make-unique.h"
+
+#include "gmock/gmock.h"
+#include <iostream>
+
+class CommentTest : public ::testing::Test {
 };
 
-TEST(comment, new_comment)
+TEST_F(CommentTest, new_comment)
 {
   Document doc;
   Comment *c;
 
   c = doc.new_comment("hello");
 
-  CHECK(c != NULL);
-  STRCMP_EQUAL("hello", c->text());
-  CHECK_EQUAL(NODE_TYPE_COMMENT, c->type());
+  ASSERT_TRUE(c != NULL);
+  ASSERT_STREQ("hello", c->text());
+  ASSERT_EQ(NODE_TYPE_COMMENT, c->type());
 
   delete c;
 }
@@ -27,5 +26,6 @@ TEST(comment, new_comment)
 int
 main(int argc, char **argv)
 {
-  return CommandLineTestRunner::RunAllTests(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
