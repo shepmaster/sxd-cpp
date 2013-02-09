e0927c5dddee84089cf3f305355602946074c30e
Jake Goulding <jake.goulding@gmail.com>
2013-02-09 14:05:41 -0500

    Convert intern test to googletest

--- intern-test.cpp
+++ intern-test.cpp
@@ -1,85 +1,73 @@
-#include <iostream>
-
 #include "intern.h"
 
-#include <CppUTest/TestHarness.h>
-#include <CppUTest/CommandLineTestRunner.h>
+#include "gmock/gmock.h"
+#include <iostream>
 
-TEST_GROUP(intern)
-{};
-
-TEST(intern, new1)
-{
+class InternTest : public ::testing::Test {
+protected:
   intern_t *i;
-  i = intern_new();
-  CHECK(i != NULL);
-  intern_free(i);
+
+  void SetUp() {
+    i = intern_new();
+  }
+
+  void TearDown() {
+    intern_free(i);
+  }
+};
+
+TEST_F(InternTest, create)
+{
+  ASSERT_NE(nullptr, i);
 }
 
-TEST(intern, const_string)
+TEST_F(InternTest, const_string)
 {
-  intern_t *i;
   const char * const input = "hello";
   const char *result;
 
-  i = intern_new();
-
   result = intern_intern(i, input);
-  CHECK(result != NULL);
-  STRCMP_EQUAL(input, result);
-  CHECK(result != input);
 
-  intern_free(i);
+  ASSERT_STREQ(input, result);
+  ASSERT_NE(result, input);
 }
 
-TEST(intern, allocated_string)
+TEST_F(InternTest, allocated_string)
 {
-  intern_t *i;
   char *input;
   const char *result;
 
-  i = intern_new();
   input = strdup("hello");
-
   result = intern_intern(i, input);
-  CHECK(result != NULL);
-  STRCMP_EQUAL(input, result);
-  CHECK(result != input);
+
+  ASSERT_STREQ(input, result);
+  ASSERT_NE(result, input);
 
   free(input);
-  intern_free(i);
 }
 
-TEST(intern, two_identical_strings)
+TEST_F(InternTest, two_identical_strings)
 {
-  intern_t *i;
   char *input1;
   char *input2;
   const char *result1;
   const char *result2;
 
-  i = intern_new();
   input1 = strdup("hello");
   input2 = strdup("hello");
-
   result1 = intern_intern(i, input1);
   result2 = intern_intern(i, input2);
 
-  CHECK(result1 != NULL);
-  CHECK(result2 != NULL);
-
-  STRCMP_EQUAL(input1, result1);
-  STRCMP_EQUAL(input2, result2);
-  STRCMP_EQUAL(result2, result1);
-  POINTERS_EQUAL(result1, result2);
+  ASSERT_STREQ(input1, result1);
+  ASSERT_STREQ(input2, result2);
+  ASSERT_STREQ(result2, result1);
+  ASSERT_EQ(result1, result2);
 
   free(input1);
   free(input2);
-  intern_free(i);
 }
 
-int
-main(int argc, char **argv)
-{
-  return CommandLineTestRunner::RunAllTests(argc, argv);
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
