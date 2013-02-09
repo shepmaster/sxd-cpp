5e22028f64e978a0be209fc4aeee461603c9a347
Jake Goulding <jake.goulding@gmail.com>
2013-02-09 14:05:41 -0500

    Convert node test to googletest

--- node-test.cpp
+++ node-test.cpp
@@ -1,140 +1,137 @@
-#include <iostream>
-
-#include <CppUTest/TestHarness.h>
-#include <CppUTest/CommandLineTestRunner.h>
+#include "node.h"
 
 #include "document.h"
-#include "node.h"
-#include "test-utilities.h"
 
-TEST_GROUP(node)
-{};
+#include "gmock/gmock.h"
+#include <iostream>
 
-TEST(node, append_child)
-{
+class NodeTest : public ::testing::Test {
+protected:
   Document doc;
+
+  Node *new_node() {
+    return doc.new_element("dummy-element");
+  }
+};
+
+TEST_F(NodeTest, append_child)
+{
   Node *parent;
   Node *child;
 
-  parent = test_helper_new_node(doc, "parent");
-  child = test_helper_new_node(doc, "child");
+  parent = new_node();
+  child = new_node();
 
   parent->append_child(child);
-  POINTERS_EQUAL(child, parent->first_child());
-  POINTERS_EQUAL(parent, child->parent());
+  ASSERT_EQ(child, parent->first_child());
+  ASSERT_EQ(parent, child->parent());
 
   delete parent;
 }
 
-TEST(node, remove_child)
+TEST_F(NodeTest, remove_child)
 {
-  Document doc;
   Node *parent;
   Node *child;
 
-  parent = test_helper_new_node(doc, "parent");
-  child = test_helper_new_node(doc, "child");
+  parent = new_node();
+  child = new_node();
 
   parent->append_child(child);
   parent->remove_child(child);
-  POINTERS_EQUAL(NULL, parent->first_child());
+  ASSERT_EQ(NULL, parent->first_child());
 
   delete parent;
   delete child;
 }
 
-TEST(node, free_child)
+TEST_F(NodeTest, free_child)
 {
-  Document doc;
   Node *parent;
   Node *child;
 
-  parent = test_helper_new_node(doc, "parent");
-  child = test_helper_new_node(doc, "child");
+  parent = new_node();
+  child = new_node();
 
   parent->append_child(child);
   delete child;
-  POINTERS_EQUAL(NULL, parent->first_child());
+  ASSERT_EQ(NULL, parent->first_child());
 
   delete parent;
 }
 
-TEST(node, sibling)
+TEST_F(NodeTest, sibling)
 {
-  Document doc;
   Node *parent;
   Node *child1;
   Node *child2;
 
-  parent = test_helper_new_node(doc, "parent");
-  child1 = test_helper_new_node(doc, "child1");
-  child2 = test_helper_new_node(doc, "child2");
+  parent = new_node();
+  child1 = new_node();
+  child2 = new_node();
 
   parent->append_child(child1);
   parent->append_child(child2);
 
-  POINTERS_EQUAL(child2, child1->next_sibling());
+  ASSERT_EQ(child2, child1->next_sibling());
 
   delete parent;
 }
 
-TEST(node, insert_next_sibling)
+TEST_F(NodeTest, insert_next_sibling)
 {
-  Document doc;
-  Node *a = test_helper_new_node(doc, "a");
-  Node *b = test_helper_new_node(doc, "b");
-  Node *c = test_helper_new_node(doc, "c");
+  Node *a = new_node();
+  Node *b = new_node();
+  Node *c = new_node();
 
   a->insert_next_sibling(c);
-  POINTERS_EQUAL(NULL, a->prev_sibling());
-  POINTERS_EQUAL(c, a->next_sibling());
-  POINTERS_EQUAL(a, c->prev_sibling());
-  POINTERS_EQUAL(NULL, c->next_sibling());
+  ASSERT_EQ(NULL, a->prev_sibling());
+  ASSERT_EQ(c, a->next_sibling());
+  ASSERT_EQ(a, c->prev_sibling());
+  ASSERT_EQ(NULL, c->next_sibling());
 
   a->insert_next_sibling(b);
-  POINTERS_EQUAL(NULL, a->prev_sibling());
-  POINTERS_EQUAL(b, a->next_sibling());
-  POINTERS_EQUAL(a, b->prev_sibling());
-  POINTERS_EQUAL(c, b->next_sibling());
-  POINTERS_EQUAL(b, c->prev_sibling());
-  POINTERS_EQUAL(NULL, c->next_sibling());
+  ASSERT_EQ(NULL, a->prev_sibling());
+  ASSERT_EQ(b, a->next_sibling());
+  ASSERT_EQ(a, b->prev_sibling());
+  ASSERT_EQ(c, b->next_sibling());
+  ASSERT_EQ(b, c->prev_sibling());
+  ASSERT_EQ(NULL, c->next_sibling());
 
   delete a;
   delete b;
   delete c;
 }
 
-TEST(node, append_child_siblings)
+TEST_F(NodeTest, append_child_siblings)
 {
-  Document doc;
-  Node *parent = test_helper_new_node(doc, "parent");
-  Node *a = test_helper_new_node(doc, "a");
-  Node *b = test_helper_new_node(doc, "b");
-  Node *c = test_helper_new_node(doc, "c");
+  Node *parent = new_node();
+  Node *a = new_node();
+  Node *b = new_node();
+  Node *c = new_node();
 
   parent->append_child(a);
-  POINTERS_EQUAL(NULL, a->prev_sibling());
-  POINTERS_EQUAL(NULL, a->next_sibling());
+  ASSERT_EQ(NULL, a->prev_sibling());
+  ASSERT_EQ(NULL, a->next_sibling());
 
   parent->append_child(b);
-  POINTERS_EQUAL(NULL, a->prev_sibling());
-  POINTERS_EQUAL(b, a->next_sibling());
-  POINTERS_EQUAL(a, b->prev_sibling());
-  POINTERS_EQUAL(NULL, b->next_sibling());
+  ASSERT_EQ(NULL, a->prev_sibling());
+  ASSERT_EQ(b, a->next_sibling());
+  ASSERT_EQ(a, b->prev_sibling());
+  ASSERT_EQ(NULL, b->next_sibling());
 
   parent->append_child(c);
-  POINTERS_EQUAL(NULL, a->prev_sibling());
-  POINTERS_EQUAL(b, a->next_sibling());
-  POINTERS_EQUAL(a, b->prev_sibling());
-  POINTERS_EQUAL(c, b->next_sibling());
-  POINTERS_EQUAL(b, c->prev_sibling());
-  POINTERS_EQUAL(NULL, c->next_sibling());
+  ASSERT_EQ(NULL, a->prev_sibling());
+  ASSERT_EQ(b, a->next_sibling());
+  ASSERT_EQ(a, b->prev_sibling());
+  ASSERT_EQ(c, b->next_sibling());
+  ASSERT_EQ(b, c->prev_sibling());
+  ASSERT_EQ(NULL, c->next_sibling());
 
   delete parent;
 }
 
-int
-main(int argc, char **argv)
-{
-  return CommandLineTestRunner::RunAllTests(argc, argv);
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
