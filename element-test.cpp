d12e8cd96fb6c04b03a0cc6fc0584ef56f07d0ac
Jake Goulding <jake.goulding@gmail.com>
2013-02-09 14:05:41 -0500

    Convert element test to googletest

--- element-test.cpp
+++ element-test.cpp
@@ -1,49 +1,44 @@
-#include <iostream>
-
-#include <CppUTest/TestHarness.h>
-#include <CppUTest/CommandLineTestRunner.h>
-
-#include "test-utilities.h"
-
-#include "document.h"
-#include "node.h"
 #include "element.h"
 
-TEST_GROUP(element)
-{
+#include "document.h"
+#include "test-utilities.h"
+
+#include "gmock/gmock.h"
+#include <iostream>
+
+class ElementTest : public ::testing::Test {
+protected:
+  Document doc;
 };
 
-TEST(element, new_element)
+TEST_F(ElementTest, new_element)
 {
-  Document doc;
-  Element *e;
+  Element *element;
   const char * const name = "hello";
 
-  e = doc.new_element(name);
-  CHECK(e != NULL);
-  STRCMP_EQUAL(e->name(), name);
-  delete e;
+  element = doc.new_element(name);
+  ASSERT_TRUE(element != NULL);
+  ASSERT_STREQ(element->name(), name);
+  delete element;
 }
 
-TEST(element, mutated_name)
+TEST_F(ElementTest, mutated_name)
 {
-  Document doc;
   Element *element;
   char * name;
- 
+
   name = strdup("hello");
   element = doc.new_element(name);
 
   name[0] = 'y';
-  STRCMP_EQUAL(element->name(), "hello");
+  ASSERT_STREQ(element->name(), "hello");
 
   delete element;
   free(name);
 }
 
-TEST(element, set_attribute)
+TEST_F(ElementTest, set_attribute)
 {
-  Document doc;
   Element *element;
   const char * const element_name = "hello";
   const char * const attr_name = "type";
@@ -52,14 +47,13 @@ TEST(element, set_attribute)
   element = doc.new_element(element_name);
 
   element->set_attribute(attr_name, attr_value);
-  STRCMP_EQUAL(element->get_attribute(attr_name), attr_value);
+  ASSERT_STREQ(element->get_attribute(attr_name), attr_value);
 
   delete element;
 }
 
-TEST(element, mutated_attribute)
+TEST_F(ElementTest, mutated_attribute)
 {
-  Document doc;
   Element *element;
   const char * const element_name = "hello";
   char *attr_name;
@@ -70,33 +64,31 @@ TEST(element, mutated_attribute)
   element = doc.new_element(element_name);
 
   element->set_attribute(attr_name, attr_value);
-  
+
   attr_name[0] = 'h';
   attr_value[0] = 'y';
-  STRCMP_EQUAL(element->get_attribute("type"), "world");
+  ASSERT_STREQ(element->get_attribute("type"), "world");
 
   delete element;
   free(attr_name);
   free(attr_value);
 }
 
-TEST(element, output_basic)
+TEST_F(ElementTest, output_basic)
 {
-  Document doc;
   Element *element;
   StringOutput out;
 
   element = doc.new_element("one");
 
   element->output(out);
-  STRCMP_EQUAL("<one />", out.string());
+  ASSERT_STREQ("<one />", out.string());
 
   delete element;
 }
 
-TEST(element, output_attribute)
+TEST_F(ElementTest, output_attribute)
 {
-  Document doc;
   Element *element;
   StringOutput out;
 
@@ -104,14 +96,13 @@ TEST(element, output_attribute)
   element->set_attribute("hi", "there");
 
   element->output(out);
-  STRCMP_EQUAL("<one hi=\"there\" />", out.string());
+  ASSERT_STREQ("<one hi=\"there\" />", out.string());
 
   delete element;
 }
 
-TEST(element, output_child)
+TEST_F(ElementTest, output_child)
 {
-  Document doc;
   Element *element;
   Element *child;
   StringOutput out;
@@ -121,13 +112,12 @@ TEST(element, output_child)
   element->append_child(child);
 
   element->output(out);
-  STRCMP_EQUAL("<one><two /></one>", out.string());
+  ASSERT_STREQ("<one><two /></one>", out.string());
 
   delete element;
 }
 
-int
-main(int argc, char **argv)
-{
-  return CommandLineTestRunner::RunAllTests(argc, argv);
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
