b0783d904740e3263faefe85ac5ad408d10ed053
Jake Goulding <jake.goulding@gmail.com>
2013-02-10 19:18:00 -0500

    Document ToStream

--- to-stream.h
+++ to-stream.h
@@ -3,8 +3,17 @@
 
 #include <iostream>
 
+/**
+ * Provides a base implementation of writing an object to a stream.
+ */
 class ToStream {
 public:
+  /**
+   * Writes this object to a stream.
+   *
+   * This is programmer-friendly output, and is not intended for
+   * end-user consumption.
+   */
   virtual std::ostream &to_stream(std::ostream &os) const;
 };
 
