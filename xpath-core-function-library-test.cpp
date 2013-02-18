9eb5e8e2308aa61e9c3bc5a3290c6c9cbc31e7ca
Jake Goulding <jake.goulding@gmail.com>
2013-02-18 18:53:31 -0500

    Ensure the core library has the correct functions

---
+++ xpath-core-function-library-test.cpp
@@ -0,0 +1,29 @@
+#include "xpath-core-function-library.h"
+
+#include "gmock/gmock.h"
+#include <iostream>
+
+class XPathCoreFunctionLibraryTest : public ::testing::Test {
+protected:
+  XPathFunctionLibrary library;
+
+  void SetUp() {
+    XPathCoreFunctionLibrary::register_functions(library);
+  }
+};
+
+TEST_F(XPathCoreFunctionLibraryTest, true)
+{
+  ASSERT_TRUE(library.has_function("true"));
+}
+
+TEST_F(XPathCoreFunctionLibraryTest, false)
+{
+  ASSERT_TRUE(library.has_function("false"));
+}
+
+int
+main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
