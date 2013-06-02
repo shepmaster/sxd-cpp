73f3e9d4d0e5a57d35fe18efc433773243f0c76c
Jake Goulding <jake.goulding@gmail.com>
2013-06-02 13:24:07 -0400

    Make early-return statements less verbose

--- node-test-element.cpp
+++ node-test-element.cpp
@@ -7,28 +7,21 @@ NodeTestElement::NodeTestElement(PrefixedName name) :
 }
 
 void NodeTestElement::test(Node *node, Nodeset &result) const {
-  if (node->type() == NODE_TYPE_ELEMENT) {
-    Element *e = dynamic_cast<Element *>(node);
+  if (node->type() != NODE_TYPE_ELEMENT) return;
 
-    if (_name.has_prefix() != e->qname().has_namespace()) {
-      return;
-    }
+  Element *e = dynamic_cast<Element *>(node);
 
-    if (_name.has_prefix()) {
-      auto prefix_uri = e->find_namespace_for_prefix(_name.prefix());
+  if (_name.has_prefix() != e->qname().has_namespace()) return;
 
-      if (! prefix_uri) {
-        return;
-      }
+  if (_name.has_prefix()) {
+    auto prefix_uri = e->find_namespace_for_prefix(_name.prefix());
 
-      if (*prefix_uri != e->qname().namespace_uri()) {
-        return;
-      }
-    }
+    if (! prefix_uri) return;
+    if (*prefix_uri != e->qname().namespace_uri()) return;
+  }
 
-    if (_name.name() == "*" || _name.name() == e->name()) {
-      result.add(e);
-    }
+  if (_name.name() == "*" || _name.name() == e->name()) {
+    result.add(e);
   }
 }
 
